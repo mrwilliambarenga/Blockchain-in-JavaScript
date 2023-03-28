4b910c0c113964b2f14aea403bbe71162799a0b8
mrwilliambarenga <williambarenga@gmail.com>
2023-03-28 19:55:28 +0100

    Change 'data' field in Block class to 'transactions'
    This will allow the Block class to support multiple transactions.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const SHA256 = require("crypto-js/sha256");
 
 class Block {
-  constructor(index, timestamp, data, previousHash = "") {
+  constructor(index, timestamp, transactions, previousHash = "") {
     this.index = index;
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
     this.nonce = 0;
@@ -15,7 +15,7 @@ class Block {
       this.index +
         this.timestamp +
         this.previousHash +
-        JSON.stringify(this.data) +
+        JSON.stringify(this.transactions) +
         this.nonce
     ).toString();
   }
