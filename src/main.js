91e8337025383ce9a4427c4e807dee42572c5aae
mrwilliambarenga <williambarenga@gmail.com>
2023-03-28 20:31:32 +0100

    Implement 'getBalanceOfAddress' function
    In a blockchain, your balance is calculated by checking every transaction and calculating all the transfers from and to your account
    
    Loop through every transaction of every block to see how the address's balance changes, and return the value

--- src/main.js
+++ src/main.js
@@ -76,6 +76,23 @@ class Blockchain {
     this.pendingTransactions.push(transaction);
   }
 
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress == address) {
+          balance -= trans.amount;
+        }
+        if (trans.toAddress == address) {
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
+
   isChainValid() {
     for (let i = 1; i < this.chain.length; i++) {
       const currentBlock = this.chain[i];
