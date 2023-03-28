1df12dab38523cafc1919dfcb128da500be0c809
mrwilliambarenga <williambarenga@gmail.com>
2023-03-28 20:52:42 +0100

    Move all classes from 'main.js' to 'blockchain.js'

--- src/main.js
+++ src/main.js
@@ -1,114 +1,32 @@
-const SHA256 = require("crypto-js/sha256");
-
-class Transaction {
-  constructor(fromAddress, toAddress, amount) {
-    this.fromAddress = fromAddress;
-    this.toAddress = toAddress;
-    this.amount = amount;
-  }
-}
-
-class Block {
-  constructor(timestamp, transactions, previousHash = "") {
-    this.timestamp = timestamp;
-    this.transactions = transactions;
-    this.previousHash = previousHash;
-    this.hash = this.calculateHash();
-    this.nonce = 0;
-  }
-
-  calculateHash() {
-    return SHA256(
-      this.index +
-        this.timestamp +
-        this.previousHash +
-        JSON.stringify(this.transactions) +
-        this.nonce
-    ).toString();
-  }
-
-  mineBlock(difficulty) {
-    while (
-      this.hash.substring(0, difficulty) != Array(difficulty + 1).join("0")
-    ) {
-      this.nonce++;
-      this.hash = this.calculateHash();
-    }
-
-    console.log("Block mined: " + this.hash);
-  }
-}
-
-class Blockchain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-    this.difficulty = 2;
-    this.pendingTransactions = [];
-    this.miningReward = 100;
-  }
-
-  createGenesisBlock() {
-    return new Block("01/01/2023", "Genesis Block", "0");
-  }
-
-  getLatestBlock() {
-    return this.chain[this.chain.length - 1];
-  }
-
-  minePendingTransactions(miningRewardAddress) {
-    let block = new Block(Date.now(), this.pendingTransactions);
-    /** In a real blockchain, miners choose which pending transactions they
-     * would like to include on the block.
-     */
-
-    block.mineBlock(this.difficulty);
-
-    console.log("Block successfully mined!");
-
-    this.chain.push(block);
-
-    this.pendingTransactions = [
-      new Transaction(null, miningRewardAddress, this.miningReward),
-    ];
-  }
-
-  createTransaction(transaction) {
-    this.pendingTransactions.push(transaction);
-  }
-
-  getBalanceOfAddress(address) {
-    let balance = 0;
-
-    for (const block of this.chain) {
-      for (const trans of block.transactions) {
-        if (trans.fromAddress == address) {
-          balance -= trans.amount;
-        }
-        if (trans.toAddress == address) {
-          balance += trans.amount;
-        }
-      }
-    }
-
-    return balance;
-  }
-
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-
-      if (currentBlock.hash != currentBlock.calculateHash()) {
-        return false;
-      }
-
-      if (currentBlock.previousHash != previousBlock.hash) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-}
-
 let barengaCoin = new Blockchain();
+
+barengaCoin.createTransaction(new Transaction("address1", "address2", 100));
+barengaCoin.createTransaction(new Transaction("address2", "address1", 50));
+
+console.log(
+  "\naddress1's balance is: " +
+    barengaCoin.getBalanceOfAddress("address1")
+);
+
+console.log(
+  "\naddress2's balance is: " +
+    barengaCoin.getBalanceOfAddress("address2")
+);
+
+console.log("\nStarting the miner...");
+barengaCoin.minePendingTransactions("william's-address");
+
+console.log(
+  "\nWilliam's balance is: " +
+    barengaCoin.getBalanceOfAddress("william's-address")
+);
+
+console.log(
+  "\naddress1's balance is: " +
+    barengaCoin.getBalanceOfAddress("address1")
+);
+
+console.log(
+  "\naddress2's balance is: " +
+    barengaCoin.getBalanceOfAddress("address2")
+);
